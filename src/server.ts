d6c8a2356b8e88f93e9551f4b712d0a2c0d8a2fb
shruti pandya <68537491+shrutipandya3@users.noreply.github.com>
2025-07-04 12:14:56 +0530

    Update server.ts for port config

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+if (isNaN(port)) {
+  throw new Error("PORT must be a number");
+}
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Express is listening on port ${port}`);
